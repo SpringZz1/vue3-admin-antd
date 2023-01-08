142c64a26e56ba8b8c9edbb728306aaf4824cd98
SpringZz <wang90944@gmail.com>
2023-01-09 00:18:23 +0800

    全局挂载message

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import { message } from "ant-design-vue";
+import "ant-design-vue/es/message/style/css";
+
 // import "./assets/main.css";
 // 重设样式
 import "@/styles/global.css";
@@ -13,5 +16,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(message);
 
 app.mount("#app");
