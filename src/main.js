819e180296b59fc46077e373de2553ef2be95996
SpringZz <wang90944@gmail.com>
2023-01-09 22:41:41 +0800

    去掉message插件的重复引用

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import { message } from "ant-design-vue";
 import "ant-design-vue/es/message/style/css";
 
 // import "./assets/main.css";
@@ -16,6 +15,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(message);
 
 app.mount("#app");
