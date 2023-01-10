305720c2c06fbe92adbf7a4180067d9ecd88a90e
SpringZz <wang90944@gmail.com>
2023-01-11 00:00:31 +0800

    localstore添加定时删除功能

--- src/utils/storage/index.js
+++ src/utils/storage/index.js
@@ -1,12 +1,31 @@
 class localCache {
-  set(key, value) {
-    window.localStorage.setItem(key, JSON.stringify(value));
+  constructor(expires) {
+    // 设置过期时间
+    this._expires = expires;
+  }
+  set(key, obj) {
+    obj.startTime = Date.now();
+    window.localStorage.setItem(key, JSON.stringify(obj));
+    console.log("set");
   }
   get(key) {
-    const value = window.localStorage.getItem(key);
-    if (value) {
-      return JSON.parse(value);
+    const obj = JSON.parse(window.localStorage.getItem(key));
+    // 如果判断obj不为null的情况
+    if (obj !== null) {
+      let now = Date.now();
+      // 如果当前时间减去生成时间超过设定保存时间则清除localstore
+      if (now - obj.startTime > this._expires) {
+        console.log("localstore保存超时已被消除");
+        // console.log(window.localStorage.getItem("key"));
+        window.localStorage.removeItem("key");
+        return obj;
+      } else {
+        // console.log(obj._value);
+        return obj._value;
+      }
     }
+    // obj为空直接返回null
+    return null;
   }
   remove(key) {
     window.localStorage.removeItem(key);
@@ -15,5 +34,5 @@ class localCache {
     window.localStorage.clear();
   }
 }
-
-export default new localCache();
+// 设置过期时间为5s, 测试使用
+export default new localCache(5000);
