0299a3f994f5faae50e47983f5661f5d917ed2c3
SpringZz <wang90944@gmail.com>
2023-01-12 21:46:48 +0800

    添加全局守卫

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/login/index.vue";
 import Layout from "../layout/index.vue";
 import WorkBench from "../views/workbench/index.vue";
+
+import localCache from "@/utils/storage";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -23,13 +25,22 @@ const router = createRouter({
       name: "login",
       component: Login,
     },
-    // {
-    //   path: "/workbench",
-    //   name: "workbench",
-    //   children: [],
-    //   component: () => import("../views/workbench/index.vue"),
-    // },
   ],
+});
+
+// 设置全局登录拦截, 本地没有loginInfo则重定位到login首页
+router.beforeEach((to, from, next) => {
+  // 判断有没有登录
+  if (!localCache.get("loginInfo")) {
+    if (to.name === "login") {
+      next();
+    } else {
+      console.log("没有localstore信息, 重定向至login页面");
+      router.push("login");
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
