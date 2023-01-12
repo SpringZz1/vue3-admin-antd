96e6ec072013d498ca980ea96688817feb32a3ae
SpringZz <wang90944@gmail.com>
2023-01-12 23:34:22 +0800

    使用mock模拟token并保存在cookie中

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Login from "../views/login/index.vue";
 import Layout from "../layout/index.vue";
 import WorkBench from "../views/workbench/index.vue";
 
-import localCache from "@/utils/storage";
+import Cookies from "js-cookie";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -30,12 +30,13 @@ const router = createRouter({
 
 // 设置全局登录拦截, 本地没有loginInfo则重定位到login首页
 router.beforeEach((to, from, next) => {
-  // 判断有没有登录
-  if (!localCache.get("loginInfo")) {
+  const token = Cookies.get("token");
+  // 如果token不存在
+  if (!token) {
     if (to.name === "login") {
       next();
     } else {
-      console.log("没有localstore信息, 重定向至login页面");
+      console.log("没有token信息, 重定向至login页面");
       router.push("login");
     }
   } else {
