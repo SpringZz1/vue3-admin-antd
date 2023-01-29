f01621c647c2a450cbcdf5c04c3fa0cd7f314109
SpringZz <wang90944@gmail.com>
2023-01-29 17:53:59 +0800

    封装token方法修改，router相关也修改

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/login/index.vue";
 import Layout from "../layout/index.vue";
 import WorkBench from "../views/workbench/index.vue";
-
-import Cookies from "js-cookie";
+import tokenCache from "../utils/token";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -30,7 +29,7 @@ const router = createRouter({
 
 // 设置全局登录拦截, 本地没有loginInfo则重定位到login首页
 router.beforeEach((to, from, next) => {
-  const token = Cookies.get("token");
+  const token = tokenCache.get("token");
   // 如果token不存在
   if (!token) {
     if (to.name === "login") {
