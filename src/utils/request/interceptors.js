8d9e355fc2e38694472a3925bf16966e945c1f48
SpringZz <wang90944@gmail.com>
2023-01-29 17:54:56 +0800

    更好的封装axios，将拦截器全部写在interceptors文件中

---
+++ src/utils/request/interceptors.js
@@ -0,0 +1,81 @@
+import TokenCache from "../token/index";
+
+export function reqResolve(config) {
+  // 处理不需要token的请求
+  if (config.noNeedToken) {
+    return config;
+  }
+
+  const token = TokenCache.get("token");
+  if (!token) {
+    return Promise.reject({ code: 401, message: "登录已过期, 请重新登录!" });
+  }
+  config.headers.Authorization =
+    config.headers.Authorization || "Bearer" + token;
+
+  return config;
+}
+
+export function reqReject(error) {
+  return Promise.reject(error);
+}
+
+export function resResolve(config) {
+  console.log("response相应拦截器");
+  return Promise.resolve(config);
+}
+
+export function resReject(error) {
+  let message = "未知错误";
+  if (error && error.response) {
+    switch (error.response.state) {
+      case 302:
+        message = "接口重定向了！";
+        break;
+      case 400:
+        message = "参数不正确！";
+        break;
+      case 401:
+        message = "您未登录，或者登录已经超时，请先登录！";
+        break;
+      case 403:
+        message = "您没有权限操作！";
+        break;
+      case 404:
+        message = `请求地址出错: ${error.response.config.url}`;
+        break; // 在正确域名下
+      case 408:
+        message = "请求超时！";
+        break;
+      case 409:
+        message = "系统已存在相同数据！";
+        break;
+      case 500:
+        message = "服务器内部错误！";
+        break;
+      case 501:
+        message = "服务未实现！";
+        break;
+      case 502:
+        message = "网关错误！";
+        break;
+      case 503:
+        message = "服务不可用！";
+        break;
+      case 504:
+        message = "服务暂时无法访问，请稍后再试！";
+        break;
+      case 505:
+        message = "HTTP版本不受支持！";
+        break;
+      default:
+        message = "异常问题，请联系管理员！";
+        break;
+    }
+  }
+  if (error.message.includes("timeout")) message = "网络请求超时！";
+  if (error.message.includes("Network"))
+    message = window.navigator.onLine ? "服务端异常！" : "您断网了！";
+  console.log(message);
+  return Promise.reject(error);
+}
