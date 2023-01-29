98d924f8f835849c1d1ec437b8ccd9afbaf31d00
SpringZz <wang90944@gmail.com>
2023-01-29 21:15:31 +0800

    完成pinia对用户信息的持久化存储

--- src/stores/modules/user/index.js
+++ src/stores/modules/user/index.js
@@ -18,13 +18,9 @@ export const useUserStore = defineStore("user", {
   actions: {
     async getUserInfo() {
       try {
-        // api.getUsers();
         const res = await api.getUsers();
-        console.log(res.data.data);
-        // TODO: 此处暂时写死，先测试功能，功能已经成功，后续修改为动态内容
-        const { id, username, avatar, password, role } = res.data.data.admin;
+        const { username, password } = res.data.data;
         this.userInfo = { username, password };
-        // console.log(this.userInfo);
         return Promise.resolve(res.data);
       } catch (error) {
         return Promise.reject(error);
