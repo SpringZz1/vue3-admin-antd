98b6844fb21cec9fc1ce52292787ab2e3b94833f
SpringZz <wang90944@gmail.com>
2023-02-02 18:03:16 +0800

    添加路由meta信息

--- src/router/index.js
+++ src/router/index.js
@@ -11,23 +11,58 @@ const router = createRouter({
       component: Layout,
       redirect: "/login",
       children: [
+        // 工作台
         {
           path: "/workbench",
           name: "workbench",
           // component: () => import("../views/workbench/index.vue"),
           component: WorkBench,
+          meta: {
+            type: "icon-zaixianxuanfang",
+            title: "工作台",
+          },
         },
+        // 外部链接, 拥有子菜单, 展示项目源码和github主页
+        {
+          path: "/externalLink",
+          name: "externalLink",
+          meta: {
+            type: "icon-lianjie",
+            title: "外部链接",
+          },
+          children: [
+            {
+              path: "https://github.com/SpringZz1/vue3-admin-antd",
+              name: "github",
+              title: "源码",
+            },
+            {
+              path: "https://github.com/SpringZz1",
+              name: "blog",
+              title: "github主页",
+            },
+          ],
+        },
+        // 404页面
         {
           path: "/404",
           name: "404",
           component: () => import("../views/error-page/index.vue"),
+          meta: {
+            type: "icon-icon-test",
+            title: "404",
+          },
         },
       ],
     },
+    // 登录页面, 如果没有输入信息强制重定位到登录页面
     {
       path: "/login",
       name: "login",
       component: Login,
+      meta: {
+        title: "login",
+      },
     },
   ],
 });
