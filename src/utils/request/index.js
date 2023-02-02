8e82af7c2afe8415617e1dc500e476e3e141bc9d
SpringZz <wang90944@gmail.com>
2023-02-02 16:35:53 +0800

    设置axios的baseURL，防止跨域问题出现

--- src/utils/request/index.js
+++ src/utils/request/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { resReject, resResolve, reqReject, reqResolve } from "./interceptors";
 const request = axios.create({
-  baseURL: "http://localhost:4000/", // url = 基本地址 + 请求网址
+  // baseURL: "http://localhost:4000/", // url = 基本地址 + 请求网址
+  baseURL: "http://127.0.0.1:4000",
   timeout: 10000, // 请求超时
 });
 
