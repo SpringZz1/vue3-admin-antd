61ae6ddf98b5a7d372f0f5cc1725481738a35b69
SpringZz <wang90944@gmail.com>
2023-02-12 15:26:27 +0800

    增加workbench的实例页面

--- src/views/workbench/index.vue
+++ src/views/workbench/index.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+import { notification } from "ant-design-vue";
+const openNotificationWithIcon = (type) => {
+  notification[type]({
+    message: "Notification Title",
+    description:
+      "This is the content of the notification. This is the content of the notification. This is the content of the notification.",
+  });
+};
+</script>
 
 <template>
-  <div>这里是workben页面</div>
+  <div>
+    <a-card title="按钮 Button" style="width: 100%">
+      <a-button @click="() => openNotificationWithIcon('success')"
+        >Success</a-button
+      >
+      <a-button @click="() => openNotificationWithIcon('info')">Info</a-button>
+      <a-button @click="() => openNotificationWithIcon('warning')"
+        >Warning</a-button
+      >
+      <a-button @click="() => openNotificationWithIcon('error')"
+        >Error</a-button
+      >
+    </a-card>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
