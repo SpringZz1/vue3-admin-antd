50b97c05b9e53dbc777af0665a053285e303b611
SpringZz <wang90944@gmail.com>
2023-02-15 11:21:52 +0800

    信息持久化store添加全局数据持久化useAppStore

--- src/stores/index.js
+++
@@ -1,7 +0,0 @@
-import { createPinia } from "pinia";
-
-export function setUpStore(app) {
-  app.use(createPinia());
-}
-
-export * from "./modules";
--- src/stores/modules/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./user";
-export * from "./app";
-export * from "./permission";
