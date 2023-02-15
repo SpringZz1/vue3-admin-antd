41a3c32788457b6468402c9fb023b4f105c69bc6
SpringZz <wang90944@gmail.com>
2023-02-15 11:24:14 +0800

    main.js重构，异步添加动态路由保证动态路由不会页面刷新而失效

--- src/main.js
+++ src/main.js
@@ -1,20 +1,33 @@
+// import { createApp } from "vue";
+// import { setUpStore } from "./store";
+
+// import App from "./App.vue";
+// import { router } from "./router";
+// import { setUpRouter } from "./router";
 import { createApp } from "vue";
-import { createPinia } from "pinia";
-
+import { setupRouter } from "@/router";
+import { setupStore } from "@/store";
 import App from "./App.vue";
-import { router } from "./router";
-
-// import "ant-design-vue/es/message/style/css";
-// import "ant-design-vue/es/modal/style/css";
 import "ant-design-vue/dist/antd.css";
 // import "./assets/main.css";
 // 重设样式
 import "@/styles/global.css";
 import "@/styles/reset.css";
 
-const app = createApp(App);
+async function setUpApp() {
+  const app = createApp(App);
+  setupStore(app);
+  await setupRouter(app);
 
-app.use(createPinia());
-app.use(router);
+  // app.use(createPinia());
 
-app.mount("#app");
+  app.mount("#app");
+}
+
+setUpApp();
+// const app = createApp(App);
+
+// app.use(createPinia());
+// app.use(router);
+
+// app.mount("#app");
